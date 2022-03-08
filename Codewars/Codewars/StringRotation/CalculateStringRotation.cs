e065e361ce8b32b37cb8f33d3574e8807e0813cb
AndreyKurilo <akurilo.aaa@gmail.com>
2022-03-08 14:57:31 +0300

    Added solution Codewars and StringRotation project

---
+++ Codewars/Codewars/StringRotation/CalculateStringRotation.cs
@@ -0,0 +1,19 @@
+﻿namespace Codewars;
+
+public class CalculateStringRotation
+{
+    public static int ShiftedDiff(string first, string second)
+    {
+        int i = 0;
+        string secondProcess;
+        while (!first.Equals(second))
+        {
+            char[] chars = second.ToCharArray();
+            secondProcess = second.Remove(0,1);
+            second = secondProcess + chars[0];
+            Console.WriteLine(second);
+            i++;
+        }
+        return i;
+    }
+}
---
+++ Codewars/Codewars/StringRotation/Program.cs
@@ -0,0 +1,11 @@
+﻿using Codewars;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int shiftNumber = CalculateStringRotation.ShiftedDiff("coffee", "eecoff");
+        Console.WriteLine("Characters we should shift the first string forward to match the second: " 
+                      + shiftNumber);
+    }
+}
