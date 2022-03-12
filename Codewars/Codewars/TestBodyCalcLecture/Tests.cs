947951e599d35b0578317702fce08a80e38c4ec9
AndreyKurilo <akurilo.aaa@gmail.com>
2022-03-12 21:49:53 +0300

    Added Test Body Calculator
    https://calc.by/weight-and-calories/body-mass-index-calculator.html

---
+++ Codewars/Codewars/TestBodyCalcLecture/Tests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        private IWebDriver _wd;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            _wd = new ChromeDriver();
+            //baseURL = "https://www.google.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                _wd.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TestBodyCalculator()
+        {
+            _wd.Navigate().GoToUrl("https://calc.by/weight-and-calories/body-mass-index-calculator.html");
+            _wd.FindElement(By.Id("bmiVar1")).Click();
+            _wd.FindElement(By.Id("bmiVar1")).Clear();
+            _wd.FindElement(By.Id("bmiVar1")).SendKeys("185");
+            _wd.FindElement(By.Id("bmiVar2")).Click();
+            _wd.FindElement(By.Id("bmiVar2")).Clear();
+            _wd.FindElement(By.Id("bmiVar2")).SendKeys("60");
+            _wd.FindElement(By.XPath("//div[@id='t3-content']/div[3]/article/section/div[2]/div[2]/div/span")).Click();
+            _wd.Close();
+        }
+    }
+}
